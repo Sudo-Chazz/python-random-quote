6de844cb01057d22550d987f87d86578ce754704
sudo-chazz <gwtpfeffer@outlook.com>
2022-01-19 16:01:37 -0500

    **added new features** --- interactivity(input # of quotes you want), remove newline chr, expanded available quotes, add quotes around quote

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,5 @@
-import random
+import secrets
+import sys
 
 def primary():
  
@@ -6,9 +7,26 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  last = len(quotes) - 1
-  rnd = random.randint(0, 13)
-  print(quotes[rnd])
+
+# ask the user how many quotes they want printed
+# also make sure the user provides an int within range
+  try:
+    num_q = int(input("Enter the number of quotes you want: "))
+    if num_q > len(quotes):
+      raise Exception()
+  except:
+    print(f"please only provide integers less than {len(quotes) + 1}")
+    sys.exit()
+    
+  
+  for num in range(num_q):
+    # random int for randomness
+    rnd = secrets.randbelow(len(quotes))
+    # remove newline character
+    quote = quotes[rnd].rstrip("\n")
+    # add quotes to quote
+    print(f"\"{quote}\"")
+
 
 if __name__== "__main__":
   primary()
